fa9fff93a5c00e8ae268812ffdc0eca4c10d2dc0
Özge Dinçer <zge Diner@DESKTOP-RRNK647>
2023-12-07 18:22:59 +0300

    Proje dosyası ekle.

---
+++ Singleton/ConfigurationService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class ConfigurationService
+    {
+
+        private static ConfigurationService instance;
+
+        public static ConfigurationService GetInstance()
+        {
+            if (instance is null)
+                instance = new();
+
+            return instance;
+        }
+
+        public string GetValue(string key)
+        {
+            ConfigurationManager configurationManager = new();
+
+            string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+
+            configurationManager.SetBasePath(path);
+
+            configurationManager.AddJsonFile("Configuration.json");
+
+            return configurationManager.GetSection(key).Value;
+        }
+    }
+}
---
+++ Static_Constructor/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine(" ");
+
+new MyClass();
+new MyClass();
+new MyClass();
+new MyClass();
+
+
+class MyClass
+{
+    static MyClass()
+    {
+        Console.WriteLine("Static Constructor");
+    }
+
+    public MyClass()
+    {
+        Console.WriteLine("Instance Created");
+    }
+}
+
+
+
